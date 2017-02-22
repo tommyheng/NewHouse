ec93122f1142fc5edbd66b67221e3cffd312a8c6
tommyheng <tommyheng@DESKTOP-KMBO2BH>
2017-02-22 23:08:30 +0800

    HttpHelper

--- House.Shell/MainWindow.xaml.cs
+++ House.Shell/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using House.Login.Views;
+﻿using FangChan.WPFModel;
+using House.Login.Views;
+using House.Utility.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,6 +33,11 @@ namespace House.Shell
         /// </summary>
         private void verifyUser()
         {
+
+            //var t = HttpClientHelper.GetResponse<LoginStateModel>
+            //(@"http://test.fang101.com/App/Login?LoginName=1&LoginPass=1");
+
+            var result = TestAPI();
             House.Login.Views.LoginView loginWindows = new LoginView();
             //登陆没有返回true,关闭了窗体，结束应用程序
             if (!loginWindows.ShowDialog() == true)
@@ -44,5 +51,19 @@ namespace House.Shell
             }
 
         }
+
+        public string TestAPI()
+        {
+            HttpHelper httpHelper = new HttpHelper();
+            UTF8Encoding utf8 = new UTF8Encoding();
+            string registerURL = "http://test.fang101.com/App/Login";
+            string postData = string.Format("LoginName={0}&LoginPass={1}", "1", "2");
+
+            httpHelper.CC = new System.Net.CookieContainer(); 
+            string html = httpHelper.PostAndGetHtml(registerURL, postData, null, null, false, Encoding.UTF8);
+            return html;
+
+        }
+
     }
 }
