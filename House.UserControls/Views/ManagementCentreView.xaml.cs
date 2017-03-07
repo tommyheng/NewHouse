9a2987892953c6cdeb12fefe06ab5d78a8ae8d3c
tommyheng <tommyheng@DESKTOP-KMBO2BH>
2017-03-07 22:37:41 +0800

    user center

--- House.UserControls/Views/ManagementCentreView.xaml.cs
+++ House.UserControls/Views/ManagementCentreView.xaml.cs
@@ -15,11 +15,14 @@ using System.Windows.Shapes;
 
 namespace House.UserControls.Views
 {
+
     /// <summary>
     /// ManagementCentreView.xaml 的交互逻辑
     /// </summary>
     public partial class ManagementCentreView
     {
+        private string newHouseKey = "";
+        private string userListKey = "";
         public ManagementCentreView()
         {
             InitializeComponent();
@@ -34,15 +37,23 @@ namespace House.UserControls.Views
         private void userCenter_Loaded(object sender, RoutedEventArgs e)
         {
             LoadData();
+            LoadCustomerListData();
         }
+
         #region 我的报备客户
         private void dataPager_PageChanging(object sender, PageChangingEventArgs e)
         {
             LoadData(myPager.NewPageIndex, 10);
         }
+
+        /// <summary>
+        /// 我报备的新房
+        /// </summary>
+        /// <param name="pageIndex"></param>
+        /// <param name="rows"></param>
         private void LoadData(int pageIndex = 1, int rows = 10)
         {
-            var result = DataRepository.Instance.GetBuildingsOrdersList2("",
+            var result = DataRepository.Instance.GetBuildingsOrdersList2(newHouseKey,
                 GlobalDataPool.Instance.Uid, pageIndex, rows);
             if (result.success)
             {
@@ -52,8 +63,33 @@ namespace House.UserControls.Views
                 listView.ItemsSource = result.data;
             }
         }
+
+        /// <summary>
+        /// 客户列表
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="pageIndex"></param>
+        /// <param name="rows"></param>
+        private void LoadCustomerListData(int pageIndex = 1, int rows = 14)
+        {
+
+            var result = DataRepository.Instance.GetBuildingsCustomerList2(userListKey, GlobalDataPool.Instance.Uid, pageIndex, rows);
+            if (result.success)
+            {
+                usersPager.TotalCount = result.TotalRows;
+                usersPager.PageIndex = pageIndex;
+                usersPager.PageSize = rows;
+                userlistView.ItemsSource = result.KeHuList;
+            }
+        }
+
+
+
         #endregion
 
-
+        private void userDataPager_PageChanging(object sender, PageChangingEventArgs e)
+        {
+            LoadCustomerListData(usersPager.NewPageIndex, 14);
+        }
     }
 }
