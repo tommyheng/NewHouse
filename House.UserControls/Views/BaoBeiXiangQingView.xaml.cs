36f17fc7b0440869ef80fb970bf8adf1f8c07b95
yuxianye <yuxianye@163.com>
2017-03-09 21:11:49 +0800

    报备详情页面添加-内容带完善

---
+++ House.UserControls/Views/BaoBeiXiangQingView.xaml.cs
@@ -0,0 +1,59 @@
+﻿using FangChan.WPFModel;
+using House.DAL;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace House.UserControls
+{
+    /// <summary>
+    /// BaoBeiXiangQingView.xaml 的交互逻辑
+    /// </summary>
+    public partial class BaoBeiXiangQingView
+    {
+        public int Bid { get; set; }
+
+        public BaoBeiXiangQingView()
+        {
+            InitializeComponent();
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData(int pageIndex = 1, int rows = 10)
+        {
+            var result = DataRepository.Instance.GetBuildingsOrdersList1(Bid,
+                GlobalDataPool.Instance.Uid, pageIndex, rows);
+            if (result.success)
+            {
+                myPager.TotalCount = result.TotalRows;
+                myPager.PageIndex = pageIndex;
+                myPager.PageSize = rows;
+                listView.ItemsSource = result.data;
+            }
+        }
+
+        private void dataPager_PageChanged(object sender, Views.PageChangedEventArgs e)
+        {
+            //LoadData(1);
+        }
+
+        private void dataPager_PageChanging(object sender, Views.PageChangingEventArgs e)
+        {
+            LoadData(myPager.NewPageIndex);
+        }
+    }
+}
